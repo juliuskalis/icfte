6b560d72cf47d098179cff0e67f31b6e4fd252cc
Julius Kalis <julius.kalis@gmail.com>
2021-12-03 18:38:43 +0100

    adds date and time to download name

--- src/app/parts/download/download.component.ts
+++ src/app/parts/download/download.component.ts
@@ -10,12 +10,14 @@ export class DownloadComponent implements OnInit {
 
   @Input() data: any;
   fileUrl: any;
+  date: string | undefined;
   constructor(private sanitizer: DomSanitizer) {  }
   ngOnInit() {
     this.downloadStuff();
   }
 
   downloadStuff() {
+    this.date = new Date().toLocaleString().replace(/:/gi, "-");
     const data = JSON.stringify(this.data);
     const blob = new Blob([data], { type: 'application/octet-stream' });
 
