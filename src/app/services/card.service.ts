3bae7a11b1828ca63b0509507fb9a409f4e4304d
Julius Kalis <julius.kalis@gmail.com>
2021-12-05 02:58:38 +0100

    adds info if no cards are loaded for learn.component.html

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -6,43 +6,44 @@ import {Card} from "../models/card";
 })
 export class CardService {
 
-  cards: Card[] = [
-    {
-      id: 1,
-      question: 'Die Frage nummer Eins behandelt ein beipiel Thema welches so so lang ist. Die Frage nummer Eins behandelt ein beipiel Thema welches so so so so lang ist. Die Frage nummer Eins behandelt ein beipiel Thema welches so so lang ist. WWW Die Frage nummer Eins behandelt ein beipiel Thema welches so so lang ist. safnweui wafbnwqiaufw qa Die Frage nummer Eins behandelt ein beipiel Thema welches so so lang ist. Die Frage nummer Eins behandelt ein beipiel Thema welches so so so so lang ist. Die Frage nummer Eins behandelt ein beipiel Thema welches so so lang ist. WWW Die Frage nummer Eins behandelt ein beipiel Thema welches so so lang ist.',
-      answer: 'Die Antwort darauf ist nicht die längste allerdings auch nicht super kurz.',
-      category: 'neu',
-      tags: []
-    },
-    {
-      id: 2,
-      question: 'wieso, weshalb, warum',
-      answer: 'Antwort 2',
-      category: 'neu',
-      tags: []
-    },
-    {
-      id: 3,
-      question: 'wer nicht fragt',
-      answer: 'Antwort 3',
-      category: 'neu',
-      tags: []
-    },
-    {
-      id: 4,
-      question: 'bleibt dumm',
-      answer: 'Antwort 4',
-      category: 'neu',
-      tags: []
-    },
-    {
-      id: 5,
-      question: 'Das hier ist die letzte Frage!',
-      answer: 'Die letzte Antwort ist die beste Antwort.',
-      category: 'neu',
-      tags: []
-    }
-  ];
+  // cards: Card[] = [
+  //   {
+  //     id: 1,
+  //     question: 'Die Frage nummer Eins behandelt ein beipiel Thema welches so so lang ist. Die Frage nummer Eins behandelt ein beipiel Thema welches so so so so lang ist. Die Frage nummer Eins behandelt ein beipiel Thema welches so so lang ist. WWW Die Frage nummer Eins behandelt ein beipiel Thema welches so so lang ist. safnweui wafbnwqiaufw qa Die Frage nummer Eins behandelt ein beipiel Thema welches so so lang ist. Die Frage nummer Eins behandelt ein beipiel Thema welches so so so so lang ist. Die Frage nummer Eins behandelt ein beipiel Thema welches so so lang ist. WWW Die Frage nummer Eins behandelt ein beipiel Thema welches so so lang ist.',
+  //     answer: 'Die Antwort darauf ist nicht die längste allerdings auch nicht super kurz.',
+  //     category: 'neu',
+  //     tags: []
+  //   },
+  //   {
+  //     id: 2,
+  //     question: 'wieso, weshalb, warum',
+  //     answer: 'Antwort 2',
+  //     category: 'neu',
+  //     tags: []
+  //   },
+  //   {
+  //     id: 3,
+  //     question: 'wer nicht fragt',
+  //     answer: 'Antwort 3',
+  //     category: 'neu',
+  //     tags: []
+  //   },
+  //   {
+  //     id: 4,
+  //     question: 'bleibt dumm',
+  //     answer: 'Antwort 4',
+  //     category: 'neu',
+  //     tags: []
+  //   },
+  //   {
+  //     id: 5,
+  //     question: 'Das hier ist die letzte Frage!',
+  //     answer: 'Die letzte Antwort ist die beste Antwort.',
+  //     category: 'neu',
+  //     tags: []
+  //   }
+  // ];
+  cards: Card[] = [];
 
   constructor() {
     const x = localStorage.getItem('cards');
