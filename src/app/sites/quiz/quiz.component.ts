d6c56e951e83e1c067c2a995bf71adb9e25e2afb
Julius Kalis <julius.kalis@gmail.com>
2021-12-05 03:02:03 +0100

    adds learn component content to quiz component

--- src/app/sites/quiz/quiz.component.ts
+++ src/app/sites/quiz/quiz.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Card} from "../../models/card";
+import {CardService} from "../../services/card.service";
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.scss']
 })
-export class QuizComponent implements OnInit {
+export class QuizComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  cardIndex: number = 0;
+  cardStep: number = 1;
+  currentAnswer: string = '';
+  hideCategory: boolean = false;
+
+  cards: Card[] = [];
+
+  constructor(private cardService: CardService) {}
 
   ngOnInit(): void {
+    this.cards = this.cardService.getCardsWithoutCategory('finished');
   }
+
+  nextCard(): void {
+    if (this.cardStep !== 3) {
+      this.cardStep++;
+    } else {
+      this.currentAnswer = '';
+      this.cardIndex++;
+      this.cardStep = 1;
+    }
+  }
+
+  stepBack(): void {
+    if (this.cardStep !== 1) {
+      this.cardStep--;
+    } else {
+      this.currentAnswer = '';
+      this.cardIndex--;
+      this.cardStep = 3;
+    }
+  }
+
+  updateCategoryTo(category: string, id: number): void {
+    this.cardService.saveCardCategory(category, id);
+  }
+
+  redo() {
+    console.log('redo');
+  }
+
+  ngOnDestroy() {}
 
 }
